dc8205e572a55a110339472cc237fd79675e7841
tmate93 <tr.mate93@gmail.com>
2020-09-23 18:53:42 +0200

    teszt

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int main()
-{
-    int a, b, sz;
-    cin >> a >> b;
-    sz=a*b;
-    cout << sz << endl;
-    return 0;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int lko = 1;
+	int& min = (a < b) ? a : b;
+	for (int i = 1; i < min; i++) 
+		if (a%i == 0 && b%i == 0) lko = i;
+	cout << lko << endl;
+	return 0;
 }
--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,7 @@
 
 IFS=$'\n'
 
-for i in `cat input.txt`; do echo "Szamok: $i, sz:" ; echo "$i" | ./a.out ; done
+for i in `cat input.txt`; do
+ echo "Szamok: $i, lko:" 
+ echo "$i" | ./a.out 
+done
