02b2e9dd1d44fd85d51ca5c7cd1a7263da8b6ba1
tmate93 <tr.mate93@gmail.com>
2020-10-11 19:36:40 +0200

    try 4

--- harc.sh
+++ harc.sh
@@ -3,9 +3,9 @@ IFS=$'\n'
 
 rm kimenetek.txt
 
-./a.out units/Hosarkany.json units/Sotetvarazslo.json >> kimenetek.txt
-./a.out units/Hosarkany.json units/Arnykiraly.json >> kimenetek.txt 
-./a.out units/Sotetvarazslo.json units/Hosarkany.json >> kimenetek.txt 
-./a.out units/Sotetvarazslo.json units/Arnykiraly.json >> kimenetek.txt 
-./a.out units/Arnykiraly.json units/Sotetvarazslo.json >> kimenetek.txt 
-./a.out units/Arnykiraly.json units/Hosarkany.json >> kimenetek.txt
+./output units/Hosarkany.json units/Sotetvarazslo.json >> kimenetek.txt
+./output units/Hosarkany.json units/Arnykiraly.json >> kimenetek.txt 
+./output units/Sotetvarazslo.json units/Hosarkany.json >> kimenetek.txt 
+./output units/Sotetvarazslo.json units/Arnykiraly.json >> kimenetek.txt 
+./output units/Arnykiraly.json units/Sotetvarazslo.json >> kimenetek.txt 
+./output units/Arnykiraly.json units/Hosarkany.json >> kimenetek.txt
