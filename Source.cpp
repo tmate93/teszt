d3e49b0846dab44ecc4cc0c39da4ec5f8832e883
tmate93 <tr.mate93@gmail.com>
2020-10-15 11:11:50 +0200

    debugging

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,9 @@
 
 int main(int argc, char **argv) {
 	try {
+	    Kalandor s1(Szorny::parseUnit(argv[1]));
 		Szorny s2 = Szorny::parseUnit(argv[2]);
-		Kalandor s1(Szorny::parseUnit(argv[1]));
+
 		while (s1.getHp() > 0 && s2.getHp() > 0) {
 			s1.tamad(s2);
 			if (s2.getHp() > 0) {
