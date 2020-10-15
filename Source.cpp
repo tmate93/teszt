1d24af1bc0eb3783f1d441b85733c8aa71e18bb5
tmate93 <tr.mate93@gmail.com>
2020-10-15 10:51:20 +0200

    test fix

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,9 @@
 
 int main(int argc, char **argv) {
 	try {
-		std::cout << argv[1] << std::endl;
-std::cout << argv[2] << std::endl;
-		//Kalandor s1(Szorny::parseUnit(argv[1])) ;
-		//Szorny s2 = Szorny::parseUnit(argv[2]);
-		/*while (s1.getHp() > 0 && s2.getHp() > 0) {
+		Szorny s2 = Szorny::parseUnit(argv[2]);
+		Kalandor s1(Szorny::parseUnit(argv[1]));
+		while (s1.getHp() > 0 && s2.getHp() > 0) {
 			s1.tamad(s2);
 			if (s2.getHp() > 0) {
 				s2.tamad(s1);
@@ -15,7 +13,7 @@ std::cout << argv[2] << std::endl;
 		}
 		if (s1.getHp() == 0) std::cout << s2.getName() << " wins. Remaining HP: " << s2.getHp() << std::endl;
 		if (s2.getHp() == 0) std::cout << s1.getName() << " wins. Remaining HP: " << s1.getHp() << ", current level: " << s1.getLvl() << ", current experience: " << s1.getXp() << std::endl;
-*/
+
 		return 0;
 
 	}
